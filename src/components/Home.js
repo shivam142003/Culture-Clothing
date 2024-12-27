d88fff689481060dc1f38922f9f7a2ca8e11f0ea
Shivam <121782205+shivam142003@users.noreply.github.com>
2024-12-27 19:06:41 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,7 +12,7 @@ export default function Home() {
                     <Carousel/>
                     <Trendingproducts/>
                     <Shopbyanime />
-                    {/* <Marquee/> */}
+                    <Marquee/>
                    
                 </div>
                
