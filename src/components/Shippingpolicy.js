5c692501f1b99053979376ff22d44f52edc45734
RiddhimaaSingh <118214685+RiddhimaaSingh@users.noreply.github.com>
2024-12-27 19:26:21 +0530

    Update Shippingpolicy.js

--- src/components/Shippingpolicy.js
+++ src/components/Shippingpolicy.js
@@ -10,7 +10,7 @@ const ShippingPolicy = () => {
 
       <h3 className="return-title">Applicability of Policy</h3>
       <p>
-        By using the Cultural Clothing Exchanges Portal and/or initiating a request for the purchase of products, you agree to the terms contained in this shipping policy without modification. If you do not agree to these terms, we advise you not to transact on culturalclothing.com.
+        By using the Culture Clothing Exchanges Portal and/or initiating a request for the purchase of products, you agree to the terms contained in this shipping policy without modification. If you do not agree to these terms, we advise you not to transact on Cultureclothing.com.
       </p>
       <p>
         Please note that we may change the terms of this policy from time to time. Every time you wish to use the portal, please check the policy to ensure you understand the terms and conditions that apply.
